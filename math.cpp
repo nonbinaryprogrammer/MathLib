cf826800a8fe3d5a390cb28a4393c1e93c75c5c4
Horváth Péter <b8horpet@gmail.com>
2016-04-13 01:35:42 +0200

    using assert instead of printf
    also print became const method

--- math.cpp
+++ math.cpp
@@ -1,530 +1,346 @@
+#include <assert.h>
+#include "string.h"
 #include "mathlib.h"
 
+template<typename T>
+void Assert_BigInt_Equals(const BigInt& tested, T expected, const char* msg=NULL)
+{
+	assert(tested==expected);
+	if(msg)
+	{
+		printf(msg,expected);
+		tested.print();
+	}
+}
+
+template<typename T>
+void Assert(T tested, T expected, const char* msg=NULL)
+{
+	assert(tested==expected);
+	if(msg)
+	{
+		printf(msg,expected,tested);
+	}
+}
+
+char buffer[128]={0};
+char* IntToString(int a)
+{
+	memset(buffer,0,128);
+	sprintf(buffer,"%d",a);
+	return buffer;
+}
+
+const int test_value[9]={2000,2111,999,1000,123,4,3,444,2};
+
 int main() {
-  int i = 2000;
+  int i = test_value[0];
   BigInt b, c;
 
   printf("testing assignment (=):\n");
   b = i;
-  printf("expected: %d, actual: ", 2000);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[0], "expected: %d, actual: " );
   c = b;
-  printf("expected: %d, actual: ", 2000);
-  c.print();
+  Assert_BigInt_Equals( c, test_value[0], "expected: %d, actual: " );
   b = (long)i;
-  printf("expected: %d, actual: ", 2000);
-  b.print();
-  b = "2000";
-  printf("expected: %d, actual: ", 2000);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[0], "expected: %d, actual: " );
+  b = IntToString(test_value[0]);
+  Assert_BigInt_Equals( b, test_value[0], "expected: %d, actual: " );
   printf("\n");
 
   printf("testing +=:\n");
   c = i;
-  c += (int)2111;
-  printf("expected: %d, actual: ", 4111);
-  c.print();
-  c += (long)2111;
-  printf("expected: %d, actual: ", 6222);
-  c.print();
-  c += "2111";
-  printf("expected: %d, actual: ", 8333);
-  c.print();
-  b = 999;
+  c += (int)test_value[1];
+  Assert_BigInt_Equals( c, test_value[0]+1*test_value[1], "expected: %d, actual: " );
+  c += (long)test_value[1];
+  Assert_BigInt_Equals( c, test_value[0]+2*test_value[1], "expected: %d, actual: " );
+  c += IntToString(test_value[1]);
+  Assert_BigInt_Equals( c, test_value[0]+3*test_value[1], "expected: %d, actual: " );
+  b = test_value[2];
   c += b;
-  printf("expected: %d, actual: ", 9332);
-  c.print();
+  Assert_BigInt_Equals( c, test_value[0]+3*test_value[1]+1*test_value[2], "expected: %d, actual: " );
   c += b;
-  printf("expected: %d, actual: ", 10331);
-  c.print();
+  Assert_BigInt_Equals( c, test_value[0]+3*test_value[1]+2*test_value[2], "expected: %d, actual: " );
   printf("\n");
 
   printf("testing +:\n");
   c = i;
-  c = c + (int)2111;
-  printf("expected: %d, actual: ", 4111);
-  c.print();
-  c = c + (long)2111;
-  printf("expected: %d, actual: ", 6222);
-  c.print();
-  c = c + "2111";
-  printf("expected: %d, actual: ", 8333);
-  c.print();
-  b = 999;
+  c = c + (int)test_value[1];
+  Assert_BigInt_Equals( c, test_value[0]+1*test_value[1], "expected: %d, actual: " );
+  c = c + (long)test_value[1];
+  Assert_BigInt_Equals( c, test_value[0]+2*test_value[1], "expected: %d, actual: " );
+  c = c + IntToString(test_value[1]);
+  Assert_BigInt_Equals( c, test_value[0]+3*test_value[1], "expected: %d, actual: " );
+  b = test_value[2];
   c = c + b;
-  printf("expected: %d, actual: ", 9332);
-  c.print();
+  Assert_BigInt_Equals( c, test_value[0]+3*test_value[1]+1*test_value[2], "expected: %d, actual: " );
   c = c + b;
-  printf("expected: %d, actual: ", 10331);
-  c.print();
+  Assert_BigInt_Equals( c, test_value[0]+3*test_value[1]+2*test_value[2], "expected: %d, actual: " );
   printf("\n");
 
   printf("testing -:\n");
-  c = 1000;
-  b = 999;
+  c = test_value[3];
+  b = test_value[2];
   c = c - b;
-  printf("expected: %d, actual: ", 1);
-  c.print();
-  b = b - 123;
-  printf("expected: %d, actual: ", 876);
-  b.print();
-  b = b - (long)123;
-  printf("expected: %d, actual: ", 753);
-  b.print();
-  b = b - "123";
-  printf("expected: %d, actual: ", 630);
-  b.print();
+  Assert_BigInt_Equals( c, test_value[3]-test_value[2], "expected: %d, actual: " );
+  b = b - test_value[4];
+  Assert_BigInt_Equals( b, test_value[2]-1*test_value[4], "expected: %d, actual: " );
+  b = b - (long)test_value[4];
+  Assert_BigInt_Equals( b, test_value[2]-2*test_value[4], "expected: %d, actual: " );
+  b = b - IntToString(test_value[4]);
+  Assert_BigInt_Equals( b, test_value[2]-3*test_value[4], "expected: %d, actual: " );
   printf("\n");
 
   printf("testing -=:\n");
-  c = 1000;
-  b = 999;
+  c = test_value[3];
+  b = test_value[2];
   c -= b;
-  printf("expected: %d, actual: ", 1);
+  Assert_BigInt_Equals( c, test_value[3]-test_value[2], "expected: %d, actual: " );
   c.print();
-  b -= 123;
-  printf("expected: %d, actual: ", 876);
+  b -= test_value[4];
+  Assert_BigInt_Equals( b, test_value[2]-1*test_value[4], "expected: %d, actual: " );
   b.print();
-  b -= (long)123;
-  printf("expected: %d, actual: ", 753);
+  b -= (long)test_value[4];
+  Assert_BigInt_Equals( b, test_value[2]-2*test_value[4], "expected: %d, actual: " );
   b.print();
-  b -= "123";
-  printf("expected: %d, actual: ", 630);
+  b -= IntToString(test_value[4]);
+  Assert_BigInt_Equals( b, test_value[2]-3*test_value[4], "expected: %d, actual: " );
   b.print();
   printf("\n");
 
   printf("testing *:\n");
-  b = 4;
-  c = 3;
+  b = test_value[5];
+  c = test_value[6];
   b = c * b;
-  printf("expected: %d, actual: ", 12);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[5]*test_value[6], "expected: %d, actual: " );
   b = c * b;
-  printf("expected: %d, actual: ", 36);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[5]*test_value[6]*test_value[6], "expected: %d, actual: " );
   b = c * b;
-  printf("expected: %d, actual: ", 108);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[5]*test_value[6]*test_value[6]*test_value[6], "expected: %d, actual: " );
   printf("\n");
 
   printf("testing *=:\n");
-  b = 4;
-  c = 3;
+  b = test_value[5];
+  c = test_value[6];
   b *= c;
-  printf("expected: %d, actual: ", 12);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[5]*test_value[6], "expected: %d, actual: " );
   b *= c;
-  printf("expected: %d, actual: ", 36);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[5]*test_value[6]*test_value[6], "expected: %d, actual: " );
   b *= c;
-  printf("expected: %d, actual: ", 108);
-  b.print();
+  Assert_BigInt_Equals( b, test_value[5]*test_value[6]*test_value[6]*test_value[6], "expected: %d, actual: " );
   printf("\n");
 
   printf("testing /:\n");
-  b = 444;
-  c = 2;
+  b = test_value[7];
+  c = test_value[8];
   b = b/c;
-  printf("expected: 222, actual: ");
-  b.print();
+  Assert_BigInt_Equals( b, test_value[7]/test_value[8], "expected: %d, actual: " );
 
-  b = 444;
-  b = b/2;
-  printf("expected: 222, actual: ");
-  b.print();
+  b = test_value[7];
+  b = b/test_value[8];
+  Assert_BigInt_Equals( b, test_value[7]/test_value[8], "expected: %d, actual: " );
 
   printf("\n");
 
   printf("testing ==:\n");
-  b = 3;
-  c = 3;
-  if(b == c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  c = test_value[6];
+  Assert( b==c, true, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b == c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[5];
+  Assert( b==c, false, "expected: %d, actual: %d\n" );
   printf("\n");
 
   printf("testing !=:\n");
-  b = 4;
-  c = 3;
-  if(b != c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[5];
+  c = test_value[6];
+  Assert( b!=c, true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b != c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b!=c, false, "expected: %d, actual: %d\n" );
   printf("\n");
 
   printf("testing >:\n");
-  b = 4;
-  c = 3;
-  if(b > c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  c = test_value[6];
+  Assert( b>c, true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b > c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>c, false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b > c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>c, false, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b > 3) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b>test_value[6], true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b > 3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b > 3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b > (long)3) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b>(long)test_value[6], true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b > (long)3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>(long)test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b > (long)3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>(long)test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b > "3") {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b>IntToString(test_value[6]), true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b > "3") {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>IntToString(test_value[6]), false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b > "3") {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>IntToString(test_value[6]), false, "expected: %d, actual: %d\n" );
+
   printf("\n");
 
   printf("testing <:\n");
-  c = 4;
-  b = 3;
-  if(b < c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  c = test_value[6];
+  Assert( b<c, true, "expected: %d, actual: %d\n" );
 
-  c = 3;
-  if(b < c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<c, false, "expected: %d, actual: %d\n" );
 
-  c = 2;
-  if(b < c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<c, false, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b < 5) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b<test_value[6], true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b < 3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b < 1) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b < (long)5) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b<(long)test_value[6], true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b < (long)3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<(long)test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b < (long)1) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<(long)test_value[6], false, "expected: %d, actual: %d\n" );
 
-  b = 4;
-  if(b < "5") {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b<IntToString(test_value[6]), true, "expected: %d, actual: %d\n" );
 
-  b = 3;
-  if(b < "3") {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<IntToString(test_value[6]), false, "expected: %d, actual: %d\n" );
 
-  b = 2;
-  if(b < "1") {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<IntToString(test_value[6]), false, "expected: %d, actual: %d\n" );
+
   printf("\n");
 
   printf("Testing >=:\n");
-  b = 4;
-  if(b >= 5) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  c = test_value[6];
+  Assert( b>=c, true, "expected: %d, actual: %d\n" );
 
-  if(b >= (long)5) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>=c, true, "expected: %d, actual: %d\n" );
 
-  if(b >= "5") {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>=c, false, "expected: %d, actual: %d\n" );
 
-  c = 5;
-  if(b >= c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b>=test_value[6], true, "expected: %d, actual: %d\n" );
 
-  if(b >= 4) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>=test_value[6], true, "expected: %d, actual: %d\n" );
 
-  if(b >= (long)4) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>=test_value[6], false, "expected: %d, actual: %d\n" );
 
-  if(b >= "4") {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b>=(long)test_value[6], true, "expected: %d, actual: %d\n" );
 
-  if(b >= b) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>=(long)test_value[6], true, "expected: %d, actual: %d\n" );
 
-  c = 3;
-  if(b >= 3) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>=(long)test_value[6], false, "expected: %d, actual: %d\n" );
 
-  if(b >= (long)3) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b>=IntToString(test_value[6]), true, "expected: %d, actual: %d\n" );
 
-  if(b >= "3") {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b>=IntToString(test_value[6]), true, "expected: %d, actual: %d\n" );
 
-  if(b >= c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b>=IntToString(test_value[6]), false, "expected: %d, actual: %d\n" );
   printf("\n");
 
   printf("Testing <=:\n");
-  if(b <= 3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  c = test_value[6];
+  Assert( b<=c, true, "expected: %d, actual: %d\n" );
 
-  if(b <= (long)3) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<=c, true, "expected: %d, actual: %d\n" );
 
-  if(b <= "3") {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<=c, false, "expected: %d, actual: %d\n" );
 
-  c = 3;
-  if(b <= c) {
-    printf("expected: 0, actual: 1\n");
-  } else {
-    printf("expected: 0, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b<=test_value[6], true, "expected: %d, actual: %d\n" );
 
-  if(b <= 4) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<=test_value[6], true, "expected: %d, actual: %d\n" );
 
-  if(b <= (long)4) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<=test_value[6], false, "expected: %d, actual: %d\n" );
 
-  if(b <= "4") {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b<=(long)test_value[6], true, "expected: %d, actual: %d\n" );
 
-  c = 4;
-  if(b <= c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<=(long)test_value[6], true, "expected: %d, actual: %d\n" );
 
-  if(b <= 5) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<=(long)test_value[6], false, "expected: %d, actual: %d\n" );
 
-  if(b <= (long)5) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]-1;
+  Assert( b<=IntToString(test_value[6]), true, "expected: %d, actual: %d\n" );
 
-  if(b <= "5") {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6];
+  Assert( b<=IntToString(test_value[6]), true, "expected: %d, actual: %d\n" );
 
-  c = 5;
-  if(b <= c) {
-    printf("expected: 1, actual: 1\n");
-  } else {
-    printf("expected: 1, actual: 0\n");
-  }
+  b = test_value[6]+1;
+  Assert( b<=IntToString(test_value[6]), false, "expected: %d, actual: %d\n" );
   printf("\n");
 
   printf("testing sumdigits()\n");
   c = 123456789;
-  b = c.sumdigits();
-  printf("expected: %d, actual: ", 45);
-  b.print();
+  Assert_BigInt_Equals( c.sumdigits(), 45, "expected: %d, actual: " );
   printf("\n");
 
   printf("testing proddigits()\n");
   c = 123456789;
-  b = c.proddigits();
-  printf("expected: %d, actual: ", 362880);
-  b.print();
+  Assert_BigInt_Equals( c.proddigits(), 362880, "expected: %d, actual: " );
   printf("\n");
 
   printf("testing push:\n");
   c = 3;
   c.push(4);
-  c.print();
+  Assert_BigInt_Equals( c, 43, "expected: %d, actual: " );
   printf("\n");
 
   printf("testing peek:\n");
-  printf("expected: 4, actual: %d\n", c.peek());
+  Assert( c.peek(), 4, "expected: %d, actual: %d\n" );
   printf("\n");
 
   printf("testing pop:\n");
-  printf("expected: 4, actual: %d\n", c.pop());
-  printf("expected: 3, actual: ");
-  c.print();
+  Assert( c.pop(), 4, "expected: %d, actual: %d\n" );
+  Assert_BigInt_Equals( c, 3, "expected: %d, actual: " );
   printf("\n");
 
   return 0;
